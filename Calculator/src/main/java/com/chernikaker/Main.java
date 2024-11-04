39b5a31088a6158a24f5e71446833dc1afe553d8
Anna Shrubok <annashrubok21@gmail.com>
2024-11-04 12:54:58 +0300

    refactor: delete useless ui

--- Calculator/src/main/java/com/chernikaker/Main.java
+++ Calculator/src/main/java/com/chernikaker/Main.java
@@ -1,7 +1,6 @@
 package com.chernikaker;
 
 import com.chernikaker.UI.AdvancedCalculatorUI;
-import com.chernikaker.UI.CalculatorUI;
 
 public class Main {
     public static void main(String[] args) {
